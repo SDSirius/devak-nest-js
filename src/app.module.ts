a1aabd59f8d77d32c0bcb49c0382cb9dd9382d1c
SDSirius <114844732+SDSirius@users.noreply.github.com>
2023-10-30 19:37:45 -0300

    commit inicial

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { UserModule } from './user/user.module';
+
+import { CarsModule } from './cars/cars.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    CarsModule,
+    AuthModule,
+    UserModule
+  ],
+  controllers: [],
+  providers: [
+    {provide: APP_GUARD, useClass: JwtAuthGuard}
+  ],
+})
+export class AppModule {}
